46e52a019bf6a0329960462f46b83e45c4221bfc
andrei-brisance <andrei@brisance.digital>
2022-07-24 15:27:03 +0300

    prettify: remove commented; update names

--- models/Cinema.js
+++ models/Cinema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema({
+const cinemaSchema = mongoose.Schema({
 	title: String,
 	tagline: String,
 	vote_average: Number,
@@ -14,22 +14,4 @@ const schema = mongoose.Schema({
 	runtime: Number
 });
 
-// {
-// 	"id": 337167,
-// 	"title": "Fifty Shades Freed",
-// 	"tagline": "Don't miss the climax",
-// 	"vote_average": 6.1,
-// 	"vote_count": 1195,
-// 	"release_date": "2018-02-07",
-// 	"poster_path": "https://image.tmdb.org/t/p/w500/3kcEGnYBHDeqmdYf8ZRbKdfmlUy.jpg",
-// 	"overview": "Believing they have left behind shadowy figures from their past, newlyweds Christian and Ana fully embrace an inextricable connection and shared life of luxury. But just as she steps into her role as Mrs. Grey and he relaxes into an unfamiliar stability, new threats could jeopardize their happy ending before it even begins.",
-// 	"budget": 55000000,
-// 	"revenue": 136906000,
-// 	"genres": [
-// 		"Drama",
-// 		"Romance"
-// 	],
-// 	"runtime": 106
-// }
-
-module.exports = mongoose.model("Cinema", schema);
+module.exports = mongoose.model("Cinema", cinemaSchema);
