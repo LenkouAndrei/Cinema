d83a31a78b8936b66a8c928838ec35621d88e566
andrei-brisance <andrei@brisance.digital>
2022-07-31 23:57:58 +0300

    22

---
+++ client/src/containers/index.ts
@@ -0,0 +1,7 @@
+export * from './footer/footer';
+export * from './header/header';
+export * from './main/main';
+export * from './wrapper/wrapper';
+export * from './error-boundary/error-boundary';
+export * from './form-page/form-page';
+export * from './modal/modal';
